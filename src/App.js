3af4222fa903e2ebc3593d7a25f9cb8793a51087
AstigmaticStudio <studioastigmatic@gmail.com>
2024-05-25 15:50:42 +0300

    ркн

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-//const tg = window.Telegram.WebApp;
-//const text = `hellooooo`
-//let score = 0
 
 function App() {
 
-//  const onClose = () => {
-//    score += 1
-//  }
-
   return (
     <div className="App">
-      уо3цаш0щвзльд
-
+      привет
     </div>
   );
 }
